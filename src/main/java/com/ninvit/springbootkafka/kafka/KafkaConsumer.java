a8fb3a2c28004460692e47ef3c9a2e10080d88fb
marceloborba <marceloborba@frwk.com.br>
2023-06-29 20:23:12 -0300

    Kafka consumer

---
+++ src/main/java/com/ninvit/springbootkafka/kafka/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.ninvit.springbootkafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "ninvit", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Received from topic -> %s", message));
+    }
+}
